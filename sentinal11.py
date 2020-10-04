48eafec777ec60588b29383e59c650f92406ee9b
dark1104 <72337522+dark1104@users.noreply.github.com>
2020-10-04 15:02:48 +0530

    Update sentinal11.py

--- sentinal11.py
+++ sentinal11.py
@@ -2,15 +2,17 @@ SentinalSearch(lst, n, num):
     last = lst[n-1]
     lst[n-1] = num
     i =0
-    whle lst[i] != num:
+    while lst[i] != num:
         i+=1
     lst[n-1] = last
     if ((i < n - 1)) or (num == lst[n-1]):
         retrn ("Number found at position " + str(i + 1))
-     
+    else:
+        return ("Number not found") 
 Student_lst = []
 number_of_elements = int(input("Enter number of students:"))
 for i in range(number_of_elements):
     roll_no = int(input("Enter roll numbers:"))
     Student_lst.append(roll_no)
 num = int(input("Enter number to be searched:"))
+print(SentinalSearch(Student_lst, number_of_elements, num))
